5d1308beffa5365dcc3b4687e1e52ca9b398eb1d
Davis Hilton <fortsillmedic2017@gmail.com>
2019-01-05 05:54:49 -0500

    completed part 2/Don't know where Migration foldr went

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.title = "Cheese Categories";
+            List <CheeseCategory> allCategories = context.Categories.ToList();
+            return View(allCategories);
+        }
+        public IActionResult Add()
+        {
+            ViewBag.title = "Add Categories";
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    ID = addCategoryViewModel.ID,
+                    Name = addCategoryViewModel.Name
+                };
+                context.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+            return View(addCategoryViewModel);
+        }
+    }
+}
--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -5,7 +5,11 @@ namespace CheeseMVC.Data
 {
     public class CheeseDbContext : DbContext
     {
-        public DbSet<Cheese> Cheeses { get; set; }
+        //These DbSets(Cheese and CheeseCategory) are related(in Same Model folder)
+        //so can put their properties in the same DbContext
+        //These will be Table names
+        public DbSet<Cheese> Cheeses { get; set; }//Has its own Controller class
+        public DbSet<CheeseCategory> Categories { get; set; }//Has its own Controller class
 
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
